95913f78f7fb6d9e712b4c5dfc9fdb29bb6821d7
jensnyder <jlsnyder09@gmail.com>
2018-09-24 11:11:58 -0400

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,14 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 0; i < arr.length; i++) {
+    for (let n = i + 1; n < arr.length; n++) {
+      let current = arr[i];
+      let smallest = current;
+      if (arr[n] < smallest) {
+        smallest = arr[n];
+        arr[i] = smallest;
+        arr[n] = current;
+      }
+    }
+  }
 }
+//not exactly right, but it works
